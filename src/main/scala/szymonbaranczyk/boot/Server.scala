4e2484743af484c2199c118e67df02475ff3b151
szymonbaranczyk <szymonbaranczyk@gmail.com>
2016-07-25 00:19:40 +0200

    Thins probably working

--- src/main/scala/szymonbaranczyk/boot/Server.scala
+++ src/main/scala/szymonbaranczyk/boot/Server.scala
@@ -14,51 +14,56 @@ import scala.io.StdIn
 /**
   * Created by Szymon on 17/07/2016.
   */
+//should all these things be defined seperately? Is it only way to expose them for testing?
+case class ClosingHandle(future: Future[ServerBinding], system: ActorSystem)
 object Server {
+  implicit val system = ActorSystem("system")
+  implicit val materializer = ActorMaterializer()
+  implicit val executionContext = system.dispatcher
 
   def main(args: Array[String]) {
-    val closingHandlers = startServer()
+    val handle = startServer()
     println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
     StdIn.readLine() // let it run until user presses return
-    stopServer(closingHandlers._1, closingHandlers._2)
+    stopServer(handle)
   }
 
   def startServer() = {
-    implicit val system = ActorSystem("system")
-    implicit val materializer = ActorMaterializer()
-    // needed for the future flatMap/onComplete in the end
-    implicit val executionContext = system.dispatcher
 
-    val greeterWebSocketService =
-      Flow[Message]
-        .mapConcat {
-          case tm: TextMessage => TextMessage(Source.single("Hello ") ++ tm.textStream) :: Nil
-          case bm: BinaryMessage =>
-            // ignore binary messages but drain content to avoid the stream being clogged
-            bm.dataStream.runWith(Sink.ignore)
-            Nil
-        }
 
-    val route =
-      pathEndOrSingleSlash {
-        get {
-          getFromResource("web/index.html")
-        }
-      } ~ {
-        getFromResourceDirectory("web")
-      } ~ path("greeter") {
-        get {
-          handleWebSocketMessages(greeterWebSocketService)
-        }
-      }
     val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
-    (bindingFuture, system)
+
+
+    ClosingHandle(bindingFuture, system)
   }
 
-  def stopServer(future: Future[ServerBinding], system: ActorSystem): Unit = {
-    implicit val executionContext = system.dispatcher
-    future
+  def route =
+    pathEndOrSingleSlash {
+      get {
+        getFromResource("web/html/index.html")
+      }
+    } ~ pathPrefix("resources") {
+      getFromResourceDirectory("web")
+    } ~ path("greeter") {
+      get {
+        handleWebSocketMessages(greeterWebSocketService)
+      }
+    }
+
+  def greeterWebSocketService =
+    Flow[Message]
+      .mapConcat {
+        case tm: TextMessage => TextMessage(Source.single("Hello ") ++ tm.textStream) :: Nil
+        case bm: BinaryMessage =>
+          // ignore binary messages but drain content to avoid the stream being clogged
+          bm.dataStream.runWith(Sink.ignore)
+          Nil
+      }
+
+  def stopServer(handle: ClosingHandle): Unit = {
+    implicit val executionContext = handle.system.dispatcher
+    handle.future
       .flatMap(_.unbind()) // trigger unbinding from the port
-      .onComplete(_ => system.terminate()) // and shutdown when done
+      .onComplete(_ => handle.system.terminate()) // and shutdown when done
   }
 }
--- src/test/scala/szymonbaranczyk/ServerSpec.scala
+++ src/test/scala/szymonbaranczyk/ServerSpec.scala
@@ -4,13 +4,61 @@ package szymonbaranczyk
   * Created by User1 on 20/07/2016.
   */
 
-import org.scalatest._
-import szymonbaranczyk.boot.Server
+import akka.http.scaladsl.model.StatusCodes.OK
+import akka.http.scaladsl.model.ws.BinaryMessage
+import akka.http.scaladsl.testkit.{ScalatestRouteTest, WSProbe}
+import akka.util.ByteString
+import org.scalatest.{FlatSpec, Matchers, _}
+import szymonbaranczyk.boot.{ClosingHandle, Server}
 
-class ServerSpec extends FlatSpec {
+import scala.concurrent.duration.DurationInt
 
-  "A Server" should "start and stop without failure" in {
-    val stopHandlers = Server.startServer()
-    Server.stopServer(stopHandlers._1, stopHandlers._2)
+class ServerSpec extends FlatSpec with BeforeAndAfterAll with ScalatestRouteTest with Matchers {
+  var stopHandlers: Option[ClosingHandle] = None
+
+  "Server" should "serve the index page on /" in {
+    Get("/") ~> Server.route ~> check {
+      status shouldBe OK
+    }
   }
+
+  "it" should "serve the index page on resources/html/index.html" in {
+    Get("/resources/html/index.html") ~> Server.route ~> check {
+      status shouldBe OK
+    }
+  }
+
+  "it" should "expose Websocket" in {
+    val wsClient = WSProbe()
+
+    // WS creates a WebSocket request for testing
+    WS("/greeter", wsClient.flow) ~> Server.route ~>
+      check {
+        // check response for WS Upgrade headers
+        isWebSocketUpgrade shouldEqual true
+
+        // manually run a WS conversation
+        wsClient.sendMessage("Peter")
+        wsClient.expectMessage("Hello Peter")
+
+        wsClient.sendMessage(BinaryMessage(ByteString("abcdef")))
+        wsClient.expectNoMessage(100.millis)
+
+        wsClient.sendMessage("John")
+        wsClient.expectMessage("Hello John")
+
+        wsClient.sendCompletion()
+        wsClient.expectCompletion()
+      }
+  }
+
+  override def beforeAll: Unit = {
+    stopHandlers = Some(Server.startServer())
+  }
+
+  override def afterAll: Unit = {
+    Server.stopServer(stopHandlers.getOrElse(fail()))
+  }
+
+
 }
