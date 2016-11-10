136ca83793c660a042c927b9e792c37c300078b1
Szymon Barańczyk <szymon.baranczyk@gmail.com>
2016-11-10 18:51:19 +0100

    Preparation for calculating bullets movement, minor fixes

--- src/main/scala/szymonbaranczyk/dataLayer/BulletActor.scala
+++ src/main/scala/szymonbaranczyk/dataLayer/BulletActor.scala
@@ -1,10 +1,8 @@
 package szymonbaranczyk.dataLayer
 
 import akka.actor.{Actor, ActorRef, PoisonPill}
-import akka.actor.Actor.Receive
 import com.typesafe.scalalogging.LazyLogging
 import szymonbaranczyk.exitFlow.PlayerData
-import szymonbaranczyk.helper.InputJsonParser
 
 /**
   * Created by SBARANCZ on 2016-11-07.
@@ -14,10 +12,9 @@ class BulletActor(id: Int, var state: BulletState, xSpeed: Int, ySpeed: Int, own
 
   override def receive = {
     case CheckCollisions(players) =>
-      for (p <- players) p._2 ! GetDataWithoutCalc()
-      if (state.x < 0 || state.y < 0 || state.x > size || state.y > size) {
-        self ! PoisonPill
-      }
+      if (state.x < 0 || state.y < 0 || state.x > size || state.y > size) self ! PoisonPill
+      else for (p <- players) p._2 ! GetDataWithoutCalc()
+
     case CalculateStep() =>
       state = BulletState(state.x + xSpeed, state.y + ySpeed)
       sender ! state
